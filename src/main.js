a2b9a8aae75fdfa751dde9d908382c9a817336fa
FabianX <fabian.k@ziggo.nl>
2019-09-07 20:19:16 +0200

    changed

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import Vue from 'vue'
+import VueResource from 'vue-resource'
 import App from './App.vue'
+
+// Use vue-resource package
+Vue.use(VueResource);
+
+// Filters
+Vue.filter('to-uppercase', function(value){
+    return value.toUpperCase();
+});
 
 new Vue({
   el: '#app',
